9fb830d0ca505b9492facab1d013d7deee5fb550
SeanBrennan645 <seanbrennan645@gmail.com>
2021-10-22 21:16:49 +0100

    Added basic movement script

---
+++ Obstacle Course/Assets/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float scale = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * scale;
+        float zValue = Input.GetAxis("Vertical") * scale;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+}
