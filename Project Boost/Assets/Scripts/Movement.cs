4bbe23a7b36e7de003de7f3b40fd1d8b78466389
SeanBrennan645 <seanbrennan645@gmail.com>
2022-01-08 19:31:26 +0000

    Added movement and other stuff

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float mainThrust = 1.0f;
+    [SerializeField] float rotationThrust = 1.0f;
+    Rigidbody rigidb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rigidb.freezeRotation = true; // freezing rotation so we can maunally rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rigidb.freezeRotation = false;
     }
 }
