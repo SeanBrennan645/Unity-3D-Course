99592c056e7298d8ea257db41bce5c1625300b06
SeanBrennan645 <seanbrennan645@gmail.com>
2022-01-16 17:26:30 +0000

    Added debug collision handler

---
+++ Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                {
+                    Debug.Log("Hit something friendly");
+                    break;
+                }
+            case "Fuel":
+                {
+                    Debug.Log("Gained some fuel!");
+                    break;
+                }
+            case "Finish":
+                {
+                    Debug.Log("Reached the end");
+                    break;
+                }
+            default:
+                {
+                    Debug.Log("Oh no hit an obstacle");
+                    break;
+                }
+        }
+    }
+}
