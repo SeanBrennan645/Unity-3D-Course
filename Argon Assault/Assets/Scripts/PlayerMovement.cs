cf21236b2c3ac34eb4d9d40ed1d447ede9f6ddb3
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-06 15:02:27 +0000

    Added basic movement for player ship

--- Argon Assault/Assets/Scripts/PlayerMovement.cs
+++ Argon Assault/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] InputAction movement;
+    [SerializeField] float controlSpeed = 30.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +25,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
-        //float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
-        //float vertialThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);
+        float xThrow = movement.ReadValue<Vector2>().x;
+        float yThrow = movement.ReadValue<Vector2>().y;
+        //float horizontalThrow = Input.GetAxis("Horizontal"); old system
+        //float vertialThrow = Input.GetAxis("Vertical"); old system
+
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float newXPos = transform.localPosition.x + xOffset;
+        float newYPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(
+            newXPos,
+            newYPos,
+            transform.localPosition.z);
     }
 }
