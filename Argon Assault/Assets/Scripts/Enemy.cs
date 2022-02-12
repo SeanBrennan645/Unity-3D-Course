9ae85ee86865f86b2be91f0a73e1a393273cf1b7
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-12 16:30:19 +0000

    Added enemy hp and hit vfx

--- Argon Assault/Assets/Scripts/Enemy.cs
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("VFX")]
     [SerializeField] GameObject deathVfx;
+    [SerializeField] GameObject hitVfx;
     [SerializeField] Transform parent;
+    [Header("Point System")]
     [SerializeField] int scoreValue = 10;
+    [SerializeField] int killValue = 50;
+    [SerializeField] int hitPoints = 3;
 
     private Scoreboard scoreboard;
 
@@ -18,18 +23,25 @@ public class Enemy : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if (hitPoints <= 0)
+        {
+            KillEnemy();
+        }
     }
 
     private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVfx, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
+        scoreboard.UpdateScore(killValue);
         Destroy(gameObject);
     }
 
     private void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVfx, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         scoreboard.UpdateScore(scoreValue);
+        hitPoints -= 1;
     }
 }
