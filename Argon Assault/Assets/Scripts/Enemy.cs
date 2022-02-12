2083d3e7d581af025d05ea1506007929e0da3bd8
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-12 16:00:19 +0000

    Added simple scoreboard system

--- Argon Assault/Assets/Scripts/Enemy.cs
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -6,11 +6,20 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVfx;
     [SerializeField] Transform parent;
+    [SerializeField] int scoreValue = 10;
+
+    private Scoreboard scoreboard;
+
+    private void Start()
+    {
+        scoreboard = FindObjectOfType<Scoreboard>();
+    }
 
     private void OnParticleCollision(GameObject other)
     {
         GameObject vfx = Instantiate(deathVfx, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
+        scoreboard.UpdateScore(scoreValue);
         Destroy(gameObject);
     }
 }
---
+++ Argon Assault/Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    int score;
+
+    public void UpdateScore(int value)
+    {
+        score += value;
+        Debug.Log($"Current Score: {score}");
+    }
+}
