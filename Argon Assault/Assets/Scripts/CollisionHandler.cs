243379a4de98016a09620f579e28bdaa06f731ba
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-12 13:17:15 +0000

    Setup simple debug collision detector

---
+++ Argon Assault/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with " + collision.gameObject.name);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered by " + other.gameObject.name);
+    }
+}
