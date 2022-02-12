56f22dc38d009bc473436a664a54c0ff1fa7bec2
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-12 15:50:17 +0000

    Added enemy death effects

--- Argon Assault/Assets/Scripts/Enemy.cs
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVfx;
+    [SerializeField] Transform parent;
+
     private void OnParticleCollision(GameObject other)
     {
+        GameObject vfx = Instantiate(deathVfx, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
