7228a2df5c6edb96e2e60d7ace2f007e785a04bf
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-17 19:11:28 +0000

    Added Coordinate Labeler

---
+++ RealmRush/Assets/Scripts/CoordinateLabeler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    private void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        displayCoordinates();
+    }
+
+    void Update()
+    {
+        if(!Application.isPlaying)
+        {
+            displayCoordinates();
+            UpdateObjectName();
+        } 
+    }
+
+    private void displayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    private void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
