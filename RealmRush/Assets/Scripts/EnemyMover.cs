980c5bf872de9857fab795c3383e7401594aeb62
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-18 19:56:36 +0000

    Added lerp and speed to enemy movement
    Also replaced wait time with a speed variable

--- RealmRush/Assets/Scripts/EnemyMover.cs
+++ RealmRush/Assets/Scripts/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1.0f;
+    [SerializeField][Range(0.0f, 5.0f)] float speed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +17,18 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPos = transform.position;
+            Vector3 endPos = waypoint.transform.position;
+            float travelPercent = 0;
+
+            transform.LookAt(endPos);
+
+            while (travelPercent < 1.0f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPos, endPos, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
