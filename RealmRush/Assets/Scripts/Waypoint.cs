d21426dcc9b2ea5fb403f421d74813c47e58bdd4
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-19 15:56:28 +0000

    Tower follows enemy as they move

--- RealmRush/Assets/Scripts/Waypoint.cs
+++ RealmRush/Assets/Scripts/Waypoint.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] bool isPlaceable;
+    [SerializeField] GameObject towerPrefab;
 
     private void OnMouseDown()
     {
         if (isPlaceable)
         {
-            Debug.Log(transform.name);
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
         }
     }
 }
