601786bc059ef207a0d2a3113f62554aaebbca99
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-20 13:23:07 +0000

    Object pool now creates endless waves of enemies

--- RealmRush/Assets/Scripts/ObjectPool.cs
+++ RealmRush/Assets/Scripts/ObjectPool.cs
@@ -1,11 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
     [SerializeField] GameObject enemyPrefab;
+    [SerializeField] int poolSize = 5;
     [SerializeField] float spawnTimer = 1.0f;
+
+    private GameObject[] pool;
+
+    private void Awake()
+    {
+        PopulatePool();
+    }
+
+    private void PopulatePool()
+    {
+        pool = new GameObject[poolSize];
+
+        for (int i = 0; i < pool.Length; i++)
+        {
+            pool[i] = Instantiate(enemyPrefab, transform);
+            pool[i].SetActive(false);
+        }
+    }
 
     void Start()
     {
@@ -16,8 +36,20 @@ public class ObjectPool : MonoBehaviour
     {
         while(true)
         {
-            Instantiate(enemyPrefab, transform);
+            EnableObjectInPool();
             yield return new WaitForSeconds(spawnTimer);
+        }
+    }
+
+    private void EnableObjectInPool()
+    {
+        for (int i = 0; i < pool.Length; i++)
+        {
+            if(pool[i].activeInHierarchy==false)
+            {
+                pool[i].SetActive(true);
+                return;
+            }
         }
     }
 }
