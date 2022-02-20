e28604a64467fceef51bd231b589015f37ebf25f
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-20 14:09:42 +0000

    Added UI text for player balance

--- RealmRush/Assets/Scripts/Bank.cs
+++ RealmRush/Assets/Scripts/Bank.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -9,25 +10,35 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentBalence;
     public int CurrentBalence { get { return currentBalence; } }
 
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     private void Awake()
     {
         currentBalence = startingBalence;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalence += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalence -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if(currentBalence < 0)
         {
             //Lose the game
             ReloadScene();
         }
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalence;
     }
 
     void ReloadScene()
