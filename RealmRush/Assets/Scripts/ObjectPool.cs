9dcc1a8ed03e05122f0b0b6558e4263e980e414c
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-20 13:10:59 +0000

    Began work on enemyspawner/objectPool
    also tidied up organisation of a few script folders

---
+++ RealmRush/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] float spawnTimer = 1.0f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while(true)
+        {
+            Instantiate(enemyPrefab, transform);
+            yield return new WaitForSeconds(spawnTimer);
+        }
+    }
+}
