70e328a558b837af7c6282bf7b07bc7feb41ee5b
SeanBrennan645 <seanbrennan645@gmail.com>
2022-02-20 19:13:47 +0000

    More work on pathfinding

---
+++ RealmRush/Assets/Scripts/Tile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] bool isPlaceable;
+    [SerializeField] Tower towerPrefab;
+
+    GridManager gridManager;
+    Vector2Int coordinates = new Vector2Int();
+
+    private void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+    }
+
+    private void Start()
+    {
+        if(gridManager != null)
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+
+            if(!isPlaceable)
+            {
+                gridManager.BlockNode(coordinates);
+            }
+        }
+    }
+
+    public bool IsPlaceable
+    {
+        get
+        {
+            return isPlaceable;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (isPlaceable)
+        {
+            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceable = !isPlaced;
+        }
+    }
+}
