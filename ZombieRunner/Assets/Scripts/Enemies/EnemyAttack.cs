287ff25c256501eeb7f0826ac32505cf54a82b13
SeanBrennan645 <seanbrennan645@gmail.com>
2022-03-31 19:16:23 +0100

    Added quick enemy attack animation event
    Just debug log atm

---
+++ ZombieRunner/Assets/Scripts/Enemies/EnemyAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float damage = 40.0f;
+
+    void Start()
+    {
+        
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null)
+        {
+            return;
+        }
+        Debug.Log("hitting the boi");
+    }
+}
