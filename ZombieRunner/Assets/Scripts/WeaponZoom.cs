062878f865da3febd9540dee95879a9e0543eab1
SeanBrennan645 <seanbrennan645@gmail.com>
2022-04-21 16:08:38 +0100

    Mouse Sensitivity changes when zoomed in

--- ZombieRunner/Assets/Scripts/WeaponZoom.cs
+++ ZombieRunner/Assets/Scripts/WeaponZoom.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] Camera fpsCamera;
     [SerializeField] float zoomedOutFOV = 60.0f;
     [SerializeField] float zoomedInFOV = 20.0f;
+    [SerializeField] float zoomOutSensitivity = 2.0f;
+    [SerializeField] float zoomInSensitivity = 0.5f;
 
     private bool zoomedInToggle = false;
+    private RigidbodyFirstPersonController fpsController;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     private void Update()
     {
@@ -18,11 +27,15 @@ public class WeaponZoom : MonoBehaviour
             {
                 zoomedInToggle = false;
                 fpsCamera.fieldOfView = zoomedOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
             }
             else
             {
                 zoomedInToggle = true;
                 fpsCamera.fieldOfView = zoomedInFOV;
+                fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomInSensitivity;
             }
         }
     }
