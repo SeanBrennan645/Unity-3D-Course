b729b87a1c9cbf7c43dd0d29257193bc2e608b10
SeanBrennan645 <seanbrennan645@gmail.com>
2022-06-26 10:59:13 +0100

    Made level improvements and player health text
    Made the level more complete by tweaking lighting and player/enemy values. Also added a player health text so the player always know their current health

--- ZombieRunner/Assets/Scripts/PlayerHealth.cs
+++ ZombieRunner/Assets/Scripts/PlayerHealth.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float healthPoints = 100.0f;
+    [SerializeField] TextMeshProUGUI healthText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthText.text = "Health: " + healthPoints.ToString();
     }
 
     public void TakeDamage(float damage)
     {
         healthPoints -= damage;
-        Debug.Log("You took a hit!");
+        healthText.text = "Health: " + healthPoints.ToString();
         if(healthPoints <= 0)
         {
+            healthText.text = "Health: 0";
             GetComponent<DeathHandler>().HandleDeath();
         }
     }
